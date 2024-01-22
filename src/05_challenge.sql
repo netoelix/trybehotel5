0c4c2f63875cb9c846df93b6043f278c98587ead
Elix Neto <neto.elix@outlook.com>
2024-01-22 16:14:29 -0300

    Fix: requisito 5

--- src/05_challenge.sql
+++ src/05_challenge.sql
@@ -1,3 +1,7 @@
-SELECT SUM(single_bed)
-FROM amenities
-WHERE breakfast = TRUE;
+SELECT COUNT(*)
+FROM rooms AS uhs
+    INNER JOIN room_amenities AS uha ON uha.room_id = uhs.id
+    INNER JOIN amenities AS ame ON uha.amenity_id = ame.id
+WHERE
+    ame.single_bed >= 1
+    AND ame.breakfast = 1
